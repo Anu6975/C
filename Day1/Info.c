4bf1638efd268456a2666bb3d5b3ed15c7ebb9f0
Anuja <badveanu04@gmail.com>
2026-04-22 14:49:44 +0530

    Create Info.c

---
+++ Day1/Info.c
@@ -0,0 +1,32 @@
+
+// **Language** 
+//         -> Langauage is a medium of communication
+//         -> If human wants to communicate with machine then computer language is needed
+
+// **Programming Language** 
+//         -> If programmer wants to pass their set of instructions to the machine then programming language is needed.
+//         -> Only Programming Language Can convert into machine language for machine understanding.
+
+
+
+// Human -> Java, c, react, python, .net  -> addition -> a=23 , b=23 , c=a+b -> compiler / Interpreter -> 01010 01011 -> communication with computer -> o/p
+
+// **Types of Language**
+//     Low Level  - Machine langues (binary nos)
+//     mid-Level  - C, C++
+//     High Level - Java Python (Human Friendly)
+
+
+
+// **Standalone Applications** 
+//     -> are the applications which needs installation to use 
+//     -> To develop Standalone Applications we use C, CPP
+//     -> OS Dependent (Need File Extentions)
+
+// ** Web Applications**
+//     -> are the applications which we can use without installation
+//     -> we use HTML, CSS, JS, React etc
+//     -> OS Independent Ex Java, .net, android
+
+// ** IDE- Integrated Development Environment **
+//     -> 
