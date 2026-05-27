6892e8994fe806eac62d3b8635aa294e9bd34010
Anu6975 <badveanu04@gmail.com>
2026-05-27 16:25:49 +0530

    Update bubble_Sort.c

--- DSA/Day1/bubble_Sort.c
+++ DSA/Day1/bubble_Sort.c
@@ -1,17 +1,7 @@
 #include<stdio.h>
 
-int main(){
-    int n;
-    printf("Enter the number of Elements: ");
-    scanf("%d", &n);
-    int arr[n+1];
-
-    for(int i=0; i<n; i++){
-        printf("Enter Element %d: ", i+1);
-        scanf("%d", &arr[i]);
-    }
-    
-    for(int i=0; i<n-1; i++){
+void bubble_sort(int n, int arr[]){
+     for(int i=0; i<n-1; i++){
         for(int j=0; j<n-1-i; j++){
             if(arr[j]>arr[j+1]){
                 int temp;
@@ -24,4 +14,18 @@ int main(){
     for(int i=0; i<n; i++){
         printf("%d ", arr[i]);
     }
+}
+
+int main(){
+    int n;
+    printf("Enter the number of Elements: ");
+    scanf("%d", &n);
+    int arr[n+1];
+
+    for(int i=0; i<n; i++){
+        printf("Enter Element %d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
+
+    bubble_sort(n, arr);
 }
