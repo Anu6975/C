626638a0404326ce96ba5724ffe09a92485b0a7e
Anu6975 <badveanu04@gmail.com>
2026-06-11 17:02:22 +0530

    Day12

--- Logic Building/Day1/addition.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    int a,b;
-    printf("Enter 2 Numbers: ");
-    scanf("%d %d", &a, &b);
-
-    printf("Addition is: %d", a+b);
-}
--- Logic Building/Day1/square.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    int a;
-    printf("Enter a Number: ");
-    scanf("%d", &a);
-
-    printf("Square of %d is %d", a, a*a);
-}
